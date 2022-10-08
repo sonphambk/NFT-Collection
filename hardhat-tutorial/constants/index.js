70ebbfd3735a586892433cac681c7c041db793f2
Son Pham <son.phambk1411@hcmut.edu.vn>
2022-10-08 16:55:31 +0700

    init commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,8 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xC8F849857b3fC393F74daA54dCe87DCF0720D44C";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xBEE13Ab5d0f35A7242359993F1Dc85e27C59224A";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL, CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
